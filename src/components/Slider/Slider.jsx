d867b3b63ec24e692e0cb468ef308dd124ab3541
Mohamed abdelhaleam <mohamedabdelhaleam13@gmail.com>
2023-09-15 12:41:56 +0300

    Create Image Slider

--- src/components/Slider/Slider.jsx
+++ src/components/Slider/Slider.jsx
@@ -1,20 +1,42 @@
-import React from 'react'
+import React, { useState } from 'react'
+import 'react-slideshow-image/dist/styles.css'
+import { Slide } from 'react-slideshow-image';
+import madin from '../../assets/images/madin.png'
+import main from '../../assets/images/main.png'
 
+
+const divStyle = {
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center',
+  backgroundSize: 'cover',
+  height: '700px'
+}
+const slideImages = [
+  {
+    url: 'https://images.unsplash.com/photo-1509721434272-b79147e0e708?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1500&q=80',
+  },
+  {
+    url: 'https://images.unsplash.com/photo-1506710507565-203b9f24669b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1536&q=80',
+  },
+  {
+    url: 'https://images.unsplash.com/photo-1536987333706-fc9adfb10d91?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1500&q=80',
+
+  },
+];
 const Slider = () => {
-    const images = [
-        "../../assets/images/madin.png",
-        "../../assets/images/main.png",
-        "../../assets/images/madin.png",
-        "../../assets/images/main.png"
-    ]
-    const imageSlider = ()=>{
-        
-    }
 
   return (
-    <div className='max-w-[1280px] m-auto h-96'>
-        <img src={imageSlider()} alt="" srcset="" className='w-full border-2 border-black h-full'/>
-    </div>
+    <div className="slide-container">
+    <Slide>
+     {slideImages.map((slideImage, index)=> (
+        <div key={index}>
+          <div style={{ ...divStyle, 'backgroundImage': `url(${slideImage.url})` }}>
+          </div>
+        </div>
+      ))} 
+    </Slide>
+  </div>
   )
 }
 
